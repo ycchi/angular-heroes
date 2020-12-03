3f95910ac5343cdc8bdd4fff67bef8faff70fab4
Young CHi <young.c.chi@gmail.com>
2020-12-03 09:35:36 -0500

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-demo'),
+      dir: require('path').join(__dirname, './coverage/angular-heros'),
       subdir: '.',
       reporters: [
         { type: 'html' },
